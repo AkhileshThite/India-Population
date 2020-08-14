09fd2d4e314c6fa93cbe5db6e1e047dd55fec667
Akhilesh Thite <68826419+AkhileshThite@users.noreply.github.com>
2020-08-14 21:45:50 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -59,4 +59,4 @@ print(regressor.predict([[n]]),"million")
 """# Accuracy"""
 
 from sklearn.metrics import r2_score
-r2_score(y_test, y_pred)
+print("Accuracy: {:.2f}%".format(r2_score(y_test, y_pred)*100))
